65fedd0cc53a27d87970eec34f11116c7f731a67
ENNEDDI-Omar <enneddiomar@gmail.com>
2025-03-19 21:12:08 +0000

    feat: Set up API configuration and project structure
    - Configure API base URL in environment files
    - Create ApiService for making HTTP requests to the backend
    - Import and provide HttpClientModule in app configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //{ path: 'questions', component: QuestionsComponent },
+  //{ path: 'favorites', component: FavoritesComponent },
+  //{ path: 'signup', component: SignupComponent },
+  //{ path: 'login', component: LoginComponent },
+  //{ path: '', redirectTo: '/questions', pathMatch: 'full' }
+];
